7b6311680f419a1b3e2060e03b331e2edd9309f3
Anisha Jain <anishajain@outlook.com>
2019-01-31 13:02:57 -0800

    lyft practice question

--- moderate.py
+++ moderate.py
@@ -84,3 +84,20 @@ def numberMax(m, n, i=0):
         return m
     
     return numberMax(m, n, i+1)
+
+# 8. Given any integer, print an English phrase that describes the integer (e.g., "One Thousand,
+# Two Hundred Thirty Four").
+def englishInt(n):
+
+
+# 9. Write methods to implement the multiply, subtract, and divide operations for integers.
+# The results of all of these are integers. Use only the add operator.
+def operations():
+
+
+# 10. Given a list of people with their birth and death years, implement a method to
+# compute the year with the most number of people alive. You may assume that all people were born
+# between 1900 and 2000 (inclusive). If a person was alive during any portion of that year, they should
+# be included in that year's count. For example, Person (birth= 1908, death= 1909) is included in the
+# counts for both 1908 and 1909.
+def livingPeople():
